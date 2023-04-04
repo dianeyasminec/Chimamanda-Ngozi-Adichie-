aa9158ffbb774bb65498d106942e0549a94b6b98
Diane <dianeyasminec60@gmail.com>
2023-04-04 11:51:27 -0400

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,29 +2,24 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
-
-
+import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
-    
-    <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Chimamanda Ngozie Adichie</Navbar.Brand>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Chimamanda Ngozi Adichie</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            {/* <Nav.Link href="/">Home</Nav.Link> */}
-            <Nav.Link href="#home">Bio</Nav.Link>
-            {/* <Nav.Link href="/favoritesSongs">Favorites Song</Nav.Link> */}
+            <Nav.Link as={Link} to="/">Bio</Nav.Link>
           </Nav>
-          <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <p className='lead'> Biography</p>
-          </Navbar.Text>
+          <Nav>
+            <Nav.Link as={Link} to="/" className="lead">Biography</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    
+      </Container>
+    </Navbar>
   )
 }
+
